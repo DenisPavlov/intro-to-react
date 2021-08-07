a6c09aec21ca26ad4848475b5c2bd7882c770c77
Denis Pavlov <denispavlov1989@gmail.com>
2021-08-07 20:48:37 +0700

    square's state

--- src/main/kotlin/components/Square.kt
+++ src/main/kotlin/components/Square.kt
@@ -1,19 +1,30 @@
 package components
 
+import kotlinx.html.js.onClickFunction
 import react.RBuilder
 import react.RComponent
 import react.RProps
 import react.RState
+import react.dom.attrs
 import react.dom.button
 
 external interface SquareProps : RProps {
     var value: Int
 }
 
-class Square(props: SquareProps) : RComponent<SquareProps, RState>(props) {
+data class SquareState(val value: String) : RState
+
+class Square(props: SquareProps) : RComponent<SquareProps, SquareState>(props) {
+    init {
+        this.state = SquareState(props.value.toString())
+    }
+
     override fun RBuilder.render() {
         button(classes = "square") {
-            +"${props.value}"
+            attrs {
+                onClickFunction = { setState(SquareState("X")) }
+            }
+            +state.value
         }
     }
 }

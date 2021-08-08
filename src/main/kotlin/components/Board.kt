74258be8298d839011990fc91d9abe6db7acc75f
Denis Pavlov <denispavlov1989@gmail.com>
2021-08-08 13:51:23 +0700

    declaring a winner

--- src/main/kotlin/components/Board.kt
+++ src/main/kotlin/components/Board.kt
@@ -29,6 +29,9 @@ class Board(props: RProps) : RComponent<RProps, BoardState>(props) {
 
     private fun handleClick(i: Int) {
         val squares = state.squares.toMutableList()
+        if (calculateWinner(squares) != "" || squares[i] != "") {
+            return
+        }
         squares[i] = if (state.xIsNext) "X" else "O"
         setState(
             BoardState(
@@ -39,7 +42,12 @@ class Board(props: RProps) : RComponent<RProps, BoardState>(props) {
     }
 
     override fun RBuilder.render() {
-        val status = "Next player: ${if (state.xIsNext) "X" else "O"}"
+        val winner = calculateWinner(state.squares)
+        val status = if (winner != "") {
+            "Winner: $winner"
+        } else {
+            "Next player: ${if (state.xIsNext) "X" else "O"}"
+        }
 
         div {
             div(classes = "status") { +status }
@@ -59,5 +67,26 @@ class Board(props: RProps) : RComponent<RProps, BoardState>(props) {
                 renderSquare(8)
             }
         }
+    }
+
+    private fun calculateWinner(squares: List<String>): String {
+        val lines = listOf(
+            Triple(0, 1, 2),
+            Triple(3, 4, 5),
+            Triple(6, 7, 8),
+            Triple(0, 3, 6),
+            Triple(1, 4, 7),
+            Triple(2, 5, 8),
+            Triple(0, 4, 8),
+            Triple(2, 4, 6),
+        )
+
+        lines.forEach { line ->
+            val (a, b, c) = line
+            if (squares[a] != "" && squares[a] == squares[b] && squares[a] == squares[c]) {
+                return squares[a]
+            }
+        }
+        return ""
     }
 }
